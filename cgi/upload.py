d6719d572cdc645d9a11528af5decc53560ee1c7
Michael Sparks <sparks,m@gmail.com>
2014-09-25 00:45:29 +0100

    Simple stub cgi script to accept a piece of code
    In particular, it takes the data, stores it and returns a value
    which allows later retrieval of the data.

---
+++ cgi/upload.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+#import cgitb
+#cgitb.enable()
+
+import json
+#import cgi
+import time
+import sys
+from filelock.filelock import FileLock
+
+raw_stdin = sys.stdin.read()
+myjson = json.loads(raw_stdin)
+
+#form = cgi.FieldStorage()
+results = {}
+#myjson = form.getvalue('repr', '')
+
+myjson = json.dumps(myjson )
+
+if myjson != "''":
+    with FileLock("../upload/highest.lock", timeout=2) as lock:
+        f = open("../upload/highest")
+        r = f.read()
+        r = r.strip()
+        top_id = int(r)
+        f.close()
+        top_id += 1
+        f = open("../upload/highest","w")
+        f.write(str(top_id))
+        f.close()
+        f = open("../upload/" + str(top_id) + ".json", "w")
+        data = { "myjson": myjson, "id": top_id }
+        f.write(json.dumps(data))
+        f.flush()
+        f.close()
+else:
+    myjson = "No data uploaded"
+    top_id = ""
+
+result = { "id" : top_id }
+
+print "Content-type: application/json"
+print "Length:", len(result)
+print
+print json.dumps(result)
