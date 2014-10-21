01e6fa176f9d8c53b77d0c246703345309a66f9d
Michael Sparks <sparks,m@gmail.com>
2014-10-21 13:04:25 +0100

    Reformat, remove irrelevant tests

--- dal/003/pixel_display.ino
+++ dal/003/pixel_display.ino
@@ -442,43 +442,42 @@ typedef void (*AppPtr_t)(void) __attribute__ ((noreturn));
 #include "atmel_bootloader.h"
 
 void bootloader_start(void) {
-  AppPtr_t BootStart = (AppPtr_t) 0x3800;
   Usb_detach();
 
   cli();
 
-  delay(200);
-  // Disable watchdog if enabled by bootloader/fuses
+  delay(200);  // Allow host machine to register detached device.
+
+  // Reset the device to a state the bootloader expects
+  // Switch off timers, etc/
 
   MCUSR &= ~(1 << WDRF);
   wdt_disable();
 
-  // Return device to power-up state
 
-  TIMSK0 = 0x00;
+  TIMSK0 = USBINT = OTGCON = OTGTCON = OTGIEN = OTGINT = PLLCSR = TCNT1L = TCNT1H = TIMSK3 = TCCR1B = TCNT4L = TCNT4H = TCCR4B = 0x00;
+
+
   USBCON = 0x20;
   UHWCON = 0x80;
   USBSTA = 0x08;
-  USBINT = 0x00;
-  OTGCON = 0x00;
-  OTGTCON = 0x00;
-  OTGIEN = 0x00;
-  OTGINT = 0x00;
-  PLLCSR  = 0x00;
-  TCNT1L  = 0x00;
-  TCNT1H  = 0x00;
-  TIMSK3  = 0x00;
-  TCCR1B  = 0x00;
+
+//  USBINT = 0x00;
+//  OTGCON = 0x00;
+//  OTGTCON = 0x00;
+//  OTGIEN = 0x00;
+//  OTGINT = 0x00;
+//  PLLCSR  = 0x00;
+//  TCNT1L  = 0x00;
+//  TCNT1H  = 0x00;
+//  TIMSK3  = 0x00;
+//  TCCR1B  = 0x00;
 
 
-  TCNT4L  = 0x00;
-  TCNT4H  = 0x00;
-  TCCR4B  = 0x00;
-
-
-  // Call the DFU bootloader
-
-  // BootStart();
+//  TCNT4L  = 0x00;
+//  TCNT4H  = 0x00;
+//  TCCR4B  = 0x00;
+ 
   run_bootloader();
 }
 
