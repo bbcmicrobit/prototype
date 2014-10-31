5121d2ad9b85574dc1fba99766caebf9815d2b33
Michael Sparks <sparks,m@gmail.com>
2014-10-31 13:14:52 +0000

    Example user code applictation
    This acts as an example/test of the DAL at a high level. It's intended to be a
    base example for the py2cc code to use to autogenerate applications.

---
+++ dal/user_code.ino
@@ -0,0 +1,24 @@
+
+#include "dal.h"
+
+void ScrollSpriteStringSpriteExample_Test18() {
+    while(1) {
+        if (getButton('A') == PRESSED) break;
+        delay(50);
+    }
+    scroll_sprite(StringSprite("HELLO WORLD"),50);
+    delay(70);
+    scroll_sprite(StringSprite(":-) (:)"),50);
+    print_message("* * ", 100);
+}
+
+void setup()
+{    //.These next three should be merged into one
+    microbug_setup();
+}
+
+void loop()
+{
+    ScrollSpriteStringSpriteExample_Test18();
+}
+
