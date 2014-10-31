199d522b677a7c8edb2acbb4e3d40e3d7441bd4e
Michael Sparks <sparks,m@gmail.com>
2014-10-31 17:09:03 +0000

    Standard loop moved into user_code

--- dal/user_code.ino
+++ dal/user_code.ino
@@ -22,3 +22,17 @@ void loop()
     ScrollSpriteStringSpriteExample_Test18();
 }
 
+int main(void)
+{
+        init();
+
+#if defined(USBCON)
+        USBDevice.attach();
+#endif
+        setup();
+        for (;;) {
+                loop();
+                if (serialEventRun) serialEventRun();
+        }
+        return 0;
+}
