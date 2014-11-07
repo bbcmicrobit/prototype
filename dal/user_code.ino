bad3d3db1e73aaa8aaba7c40ca211ac670696c19
Michael Sparks <sparks,m@gmail.com>
2014-11-07 10:07:59 +0000

    Change test behaviour to allow toggling of eyes :-)

--- dal/user_code.ino
+++ dal/user_code.ino
@@ -1,20 +1,40 @@
 
 #include "dal.h"
 
+unsigned long time;
+unsigned long lasttime;
+int buttondown;
+
 void ScrollSpriteStringSpriteExample_Test18() {
-    while(1) {
-        if (getButton('A') == PRESSED) break;
-        delay(50);
-    }
     scroll_sprite(StringSprite("HELLO WORLD"),50);
     delay(70);
     scroll_sprite(StringSprite(":-) (:)"),50);
     print_message("* * ", 100);
+    while(1) {
+        if (getButton('A') == PRESSED) break;
+        if (getButton('B') == PRESSED) {
+            buttondown = 1;
+        } else {
+            if (buttondown == 1) { // Button released -- may be nice to wrap this up
+                time = millis();
+                if (time-lasttime > 10) { // Debounce
+                    toggle_eye('A');
+                    toggle_eye('B');
+                    lasttime = time;
+                    buttondown = 0;
+                }
+            }
+        }
+        delay(50);
+    }
 }
 
 void setup()
 {    //.These next three should be merged into one
     microbug_setup();
+    lasttime = 0;
+    time = 0;
+    buttondown = 0;
 }
 
 void loop()
@@ -31,6 +51,7 @@ int main(void)
 #endif
         setup();
 set_eye('L', LOW);
+set_eye('R', LOW);
 // set_eye('R', LOW);
         for (;;) {
                 loop();
