64329155835157701fb8a49259777062f1fdd431
Paul Golds <Paul.Golds@GMail.com>
2014-11-10 14:43:00 +0000

    Blockly Editor
    The Blockly Editor has now been plugged in and is working happily.

---
+++ website/app/microbug/static/microbug/js/test.js
@@ -0,0 +1,44 @@
+function enablePageInteraction() {
+    $('#editor-tabs').tab();
+    console.log("Tabs enabled on ", $('#editor-tabs'))
+
+    $('#buildCode').click(function() {
+        console.log("Building code");
+        compileBlockly();
+    })
+
+}
+
+function setupBlockly() {
+    Blockly.inject(document.getElementById('blockly'),
+        {
+            path: '/static/microbug/blockly/',
+            toolbox: document.getElementById('toolbox'),
+            trashcan: false
+        });
+}
+
+function compileBlockly() {
+    var code = Blockly.Python.workspaceToCode();
+
+    var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    var xml_text = Blockly.Xml.domToText(xml);
+
+
+    $("#code").html("<P><PRE>" + code + "</PRE>");
+    $.ajax({
+        type: "POST",
+        url: "/microbug/compile_blockly",
+        data: JSON.stringify({"repr": {"code": code, "xml": xml_text}}),
+        success: function (data) {
+            var someid = data["id"];
+            text = '<P>Link to this version - <a href="/blockly_reload.html?id=' + someid + '"> ' + someid.toString() + ' </a>';
+            $("#resultblock").html(text);
+        }
+    });
+}
+
+console.log("Loaded test page");
+enablePageInteraction();
+setupBlockly();
+
