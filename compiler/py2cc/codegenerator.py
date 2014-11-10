e33e579b5fc561cd718172ad5bd0f178c010b437
Michael Sparks <sparks,m@gmail.com>
2014-11-10 00:48:32 +0000

    Initial support for function calls.
    Nested function calls work too :)

--- compiler/py2cc/codegenerator.py
+++ compiler/py2cc/codegenerator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import sys
+
 print_int = """void print(int value) {
     char temp_str[11];
     itoa (value, temp_str, 10); 
@@ -78,7 +80,12 @@ class CodeGenerator(object):
         assert statement[0] == "statement"
         the_statement = statement[1]
         if the_statement[0] == "expression":
-            expression_type, expression_fragment = self.expression(the_statement)
+            gen_result = self.expression(the_statement)
+            try:
+                expression_type, expression_fragment = gen_result
+            except ValueError:
+                print repr(gen_result),gen_result.__class__
+                raise
             return expression_fragment
 
         if the_statement[0] == "print_statement":
@@ -92,7 +99,8 @@ class CodeGenerator(object):
         what_to_print = print_statement[1]
 
         if what_to_print[0] == "expression":
-            (expression_type, expression_fragment) = self.expression(what_to_print)
+            gen_result = self.expression(what_to_print)
+            (expression_type, expression_fragment) = gen_result
             if expression_type == "string":
                 self.need_print_str = True
 
@@ -110,7 +118,55 @@ class CodeGenerator(object):
         if the_expression[0] == "literalvalue":
             (literalvalue_type, literalvalue_fragment) = self.literalvalue(the_expression)
             return (literalvalue_type, literalvalue_fragment)
-        return ("tbd", "//TBD (expression)")
+
+        if the_expression[0] == "expression":
+            gen_result = self.expression(the_expression)
+            expression_type, expression_fragment = gen_result
+            return expression_type, expression_fragment
+
+        if the_expression[0] == "func_call":
+            sys.stderr.write("AHA\n")
+            sys.stderr.flush()
+            gen_result = self.func_call(the_expression)
+            func_type, expression_fragment = gen_result
+            return func_type, expression_fragment
+
+        sys.stderr.write("TBD::\n")
+        sys.stderr.flush()
+        sys.stderr.write(repr(expression))
+        sys.stderr.flush()
+        return ("tbd", repr(expression))
+
+    def func_call(self, func_call):
+        
+        # ['func_call', 'scroll_string', ['expr_list', ['expression', ['literalvalue', ['string', 'HELLO WORLD']]]]]
+
+        assert func_call[0] == "func_call"
+        sys.stderr.write( repr(func_call) )
+        sys.stderr.write("\n")
+        sys.stderr.flush()
+        function_name = func_call[1]
+        function_arglist = func_call[2]
+        gen_function_arglist = self.expr_list(function_arglist)
+        gen_function_arglist_ = ",".join(gen_function_arglist)
+        assert function_arglist[0] == "expr_list"
+        function_call = function_name + '("HELLO WORLD")'
+        function_call = function_name + "( " + gen_function_arglist_ + " )"
+        return ("void", function_call)
+
+    def expr_list(self, expr_list):
+        assert expr_list[0] == "expr_list"
+        list_result = []
+        for item in expr_list[1:]:
+            if item[0] == "expression":
+                gen_result = self.expression(item)
+                expression_type, expression_fragment = gen_result
+                list_result.append( expression_fragment )
+            if item[0] == "expr_list":
+                cdr_list = self.expr_list(item)
+                list_result = list_result+cdr_list
+
+        return list_result
 
     def literalvalue(self, literalvalue):
         assert literalvalue[0] == "literalvalue"
