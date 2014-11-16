f9a567af85134de3e7e07f8d6cdc93c605d6da82
Michael Sparks <sparks,m@gmail.com>
2014-11-16 01:43:18 +0000

    Generate python code and interpret code cleanly
    Run in a non-blocking fashion. The interpreter now runs in steps.
    The interpreter checks to see if the micro device is ready. If
    it is, it issues a new command. If it isn't it pauses briefly.
    
    This allows emulation of blocking behaviour.

--- website/docs/blockly/generators/python/microbug.js
+++ website/docs/blockly/generators/python/microbug.js
@@ -1,87 +1,87 @@
 'use strict';
 
-goog.provide('Blockly.JavaScript.microbug');
+goog.provide('Blockly.Python.microbug');
 
-goog.require('Blockly.JavaScript');
+goog.require('Blockly.Python');
 
 var prefix = ''; //any prefix can be added to the function names, just in case they end up being namespaced or in an object
 
-Blockly.JavaScript['microbug_scrollstring'] = function(block) {
+Blockly.Python['microbug_scrollstring'] = function(block) {
   var value_message = Blockly.JavaScript.valueToCode(block, 'Message', Blockly.JavaScript.ORDER_ATOMIC);
   var value_speed = Blockly.JavaScript.valueToCode(block, 'Speed', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'scrollString(' + value_message + ',' + value_speed + ');';
+  var code = 'scrollString(' + value_message + ',' + value_speed + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_seteye'] = function(block) {
+Blockly.Python['microbug_seteye'] = function(block) {
   var value_id = Blockly.JavaScript.valueToCode(block, 'id', Blockly.JavaScript.ORDER_ATOMIC);
   var value_state = Blockly.JavaScript.valueToCode(block, 'state', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'setEye(' + value_id + ',' + value_state + ');';
+  var code = 'setEye(' + value_id + ',' + value_state + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_eyeon'] = function(block) {
+Blockly.Python['microbug_eyeon'] = function(block) {
   var value_id = Blockly.JavaScript.valueToCode(block, 'id', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'eyeOn(' + value_id + ');';
+  var code = 'eyeOn(' + value_id + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_eyeoff'] = function(block) {
+Blockly.Python['microbug_eyeoff'] = function(block) {
   var value_id = Blockly.JavaScript.valueToCode(block, 'id', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'eyeOff(' + value_id + ');';
+  var code = 'eyeOff(' + value_id + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_printmessage'] = function(block) {
+Blockly.Python['microbug_printmessage'] = function(block) {
   var value_message = Blockly.JavaScript.valueToCode(block, 'message', Blockly.JavaScript.ORDER_ATOMIC);
   var value_pausetime = Blockly.JavaScript.valueToCode(block, 'pausetime', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'printMessage(' + value_message + ',' + value_pausetime + ');';
+  var code = 'printMessage(' + value_message + ',' + value_pausetime + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_showletter'] = function(block) {
+Blockly.Python['microbug_showletter'] = function(block) {
   var value_letter = Blockly.JavaScript.valueToCode(block, 'letter', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'showLetter(' + value_letter + ');';
+  var code = 'showLetter(' + value_letter + ')\n';
   return prefix+code;
 }
 
-Blockly.JavaScript['microbug_getbutton'] = function(block) {
+Blockly.Python['microbug_getbutton'] = function(block) {
   var value_id = Blockly.JavaScript.valueToCode(block, 'id', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'getButton(' + value_id + ');';
+  var code = 'getButton(' + value_id + ')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [prefix+code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['microbug_cleardisplay'] = function(block) {
+Blockly.Python['microbug_cleardisplay'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = 'clearDisplay();';
+  var code = 'clearDisplay()\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_plot'] = function(block) {
+Blockly.Python['microbug_plot'] = function(block) {
   var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
   var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'DALJS.plot(' + value_x + ',' + value_y + ');';
+  var code = 'DALJS.plot(' + value_x + ',' + value_y + ')\n';
   return prefix+code;
 };
 
-Blockly.JavaScript['microbug_unplot'] = function(block) {
+Blockly.Python['microbug_unplot'] = function(block) {
   var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
   var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = 'unplot(' + value_x + ',' + value_y + ');';
+  var code = 'unplot(' + value_x + ',' + value_y + ')\n';
   return prefix+codecode;
 };
 
-Blockly.JavaScript['microbug_point'] = function(block) {
+Blockly.Python['microbug_point'] = function(block) {
   var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
   var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
