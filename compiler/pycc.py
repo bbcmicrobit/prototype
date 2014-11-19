d5e41d010f7e3081278b578d6b23456e35f31aa4
Michael Sparks <sparks.m@gmail.com>
2014-11-19 00:50:50 +0000

    Move the tests behaviour to pycc_test.py and pycc.py becomes the single file compiler

--- compiler/pycc.py
+++ compiler/pycc.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
 import sys
-from py2cc.main import main
+from py2cc.main import main_single
 
-main(sys.argv[1:])
+def single_usage(prog):
+
+    print "Usage:"
+    print
+    print prog, "source/filename.py dest/filename.hex build_directory"
+
+if __name__ == "__main__":
+    if len(sys.argv) <4:
+        single_usage(prog)
+        sys.exit(1)
+
+    prog = sys.argv[0]
+    source_file = sys.argv[1]
+    dest_file = sys.argv[2]
+    build_directory = sys.argv[3]
+
+    main_single(source_file, dest_file, build_directory)
---
+++ compiler/pycc_test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import sys
+from py2cc.main import main_test
+
+main_test(sys.argv[1:])
