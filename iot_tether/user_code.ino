71fec582dc35292c6dfbd9f4090f1bf872c9e0db
Michael Sparks <sparks.m@gmail.com>
2014-11-19 04:24:39 +0000

    First iteration of the IOToy tethering code

--- iot_tether/user_code.ino
+++ iot_tether/user_code.ino
@@ -1,182 +1,54 @@
 
 #include "dal.h"
 
-// Robot Spoke for a constrained device
-
 #include <CommandHostTiny.h>
 
-const int DRIVE_FORWARD_TIME_MS = 1500;
-const int TURN_TIME_MS = 2000;
-
-class TestHost : public CommandHostTiny {
+class TetherHost : public CommandHostTiny {
 private:
-  int drive_forward_time_ms; // Advertised attribute - type int
-  int turn_time_ms;          // Advertised attribute - type int
-
-  char str_id[32];           // Advertised attribute - type char* (str)
-  double ratio;              // Advertised attribute - type double
-  bool some_flag;            // Advertised attribute - type bool
-
-
-//   void set_eye(char id, int state);
-//   void showLetter(char c);
-//   int getButton(char id);
-//   void clear_display();
-//   void plot(int x, int y);
-//   void unplot(int x, int y);
-//   int point(int x, int y);
-//   void set_display(int sprite[5][5]);
-//   void showViewport(Image& someImage, int x, int y);
-//   void ScrollImage(Image someImage, boolean loop, int trailing_spaces);
-//   int image_point(Image& someImage, int x, int y);
-//   void set_image_point(Image& someImage, int x, int y, int value);
-//   void showLetter(char c); // Could be just internal, but useful.
-//   void print_message(const char * message, int pausetime);
-//   void toggle_eye(char id);
-//
-//   struct StringSprite;
-//   void scroll_sprite(StringSprite theSprite, int pausetime);
-
 
   char temp_str[128];
 public:
-  TestHost() : drive_forward_time_ms(DRIVE_FORWARD_TIME_MS), turn_time_ms(TURN_TIME_MS), ratio(0), some_flag(true) {
-      strcpy(str_id, "default");
-  }
-  ~TestHost() { }
+  TetherHost()  { }
+  ~TetherHost() { }
 
   const char *hostid() { 
     return "testhosttiny";
   }
 
   const char * attrs() {
-    return "drive_forward_time_ms:int,turn_time_ms:int,str_id:str,ratio:float,some_flag:bool";
+    return "";
   }
   const char * funcs() {
-    return "barecommand,one_arg_int,one_arg_bool,one_arg_str,one_arg_float,one_arg_T,no_arg_result_int,no_arg_result_bool,no_arg_result_str,no_arg_result_float,no_arg_result_T,one_arg_int_result_int";
+    return "eyeon,eyeoff,scrollstring";
   }
 
   bool has_help(char * name) {
-    if (strcmp(name,"drive_forward_time_ms")==0) return true;
-    if (strcmp(name,"turn_time_ms")==0) return true;
 
-    if (strcmp(name,"ratio")==0) return true;
-    if (strcmp(name,"some_flag")==0) return true;
-    if (strcmp(name,"str_id")==0) return true;
-
-    if (strcmp(name,"one_arg_int")==0) return true;
-    if (strcmp(name,"one_arg_bool")==0) return true;
-    if (strcmp(name,"one_arg_str")==0) return true;
-    if (strcmp(name,"one_arg_float")==0) return true;
-    if (strcmp(name,"one_arg_T")==0) return true;
-    if (strcmp(name,"barecommand")==0) return true;
-
-    if (strcmp(name,"no_arg_result_int")==0) return true;
-    if (strcmp(name,"no_arg_result_bool")==0) return true;
-    if (strcmp(name,"no_arg_result_str")==0) return true;
-    if (strcmp(name,"no_arg_result_float")==0) return true;
-    if (strcmp(name,"no_arg_result_T")==0) return true;
-
-    if (strcmp(name,"one_arg_int_result_int")==0) return true;
+    if (strcmp(name,"eyeon")==0) return true;
+    if (strcmp(name,"eyeoff")==0) return true;
+    if (strcmp(name,"scrollstring")==0) return true;
 
     return false;
   }
 
   void help(char * name) {
-    if (strcmp(name,"drive_forward_time_ms")==0) Serial.println(F("int - How long to move forward"));
-    else if (strcmp(name,"turn_time_ms")==0) Serial.println(F("int - How long to turn"));
 
-    else if (strcmp(name,"ratio")==0) Serial.println(F("float - Sample float attribute"));
-    else if (strcmp(name,"some_flag")==0) Serial.println(F("bool - Sample bool attribute"));
-    else if (strcmp(name,"str_id")==0) Serial.println(F("str - Sample str attribute"));
+    if (strcmp(name,"eyeon")==0) Serial.println(F("eyeon myarg:str -> - Switch eye on L or R"));
+    else if (strcmp(name,"eyeoff")==0) Serial.println(F("eyeoff myarg:str -> - Switch eye off L or R"));
+    else if (strcmp(name,"scrollstring")==0) Serial.println(F("scrollstring myarg:str -> - Scroll a message"));
 
-    else if (strcmp(name,"barecommand")==0) Serial.println(F("barecommand -> - test, basic command, no arg/result"));
-    else if (strcmp(name,"one_arg_int")==0) Serial.println(F("one_arg_int myarg:int -> - test, one arg, integer"));
-    else if (strcmp(name,"one_arg_bool")==0) Serial.println(F("one_arg_bool myarg:bool -> - test, one arg, boolean"));
-    else if (strcmp(name,"one_arg_str")==0) Serial.println(F("one_arg_str myarg:str -> - test, one arg, string"));
-    else if (strcmp(name,"one_arg_float")==0) Serial.println(F("one_arg_float myarg:float -> - test, one arg, float"));
-    else if (strcmp(name,"one_arg_T")==0) Serial.println(F("one_arg_T attr:T -> - test, one arg, generic type"));
-
-    else if (strcmp(name,"no_arg_result_int")==0) Serial.println(F("no_arg_result_int -> result:int - test, one arg, integer"));
-    else if (strcmp(name,"no_arg_result_bool")==0) Serial.println(F("no_arg_result_bool -> result:bool - test, one arg, boolean"));
-    else if (strcmp(name,"no_arg_result_str")==0) Serial.println(F("no_arg_result_str -> result:str - test, one arg, string"));
-    else if (strcmp(name,"no_arg_result_float")==0) Serial.println(F("no_arg_result_float -> result:float - test, one arg, float"));
-    else if (strcmp(name,"no_arg_result_T")==0) Serial.println(F("no_arg_result_T -> result:T - test, one arg, generic type"));
-
-    else if (strcmp(name,"one_arg_int_result_int")==0) Serial.println(F("one_arg_int_result_int myarg:int -> result:int - test, one arg, one result, both ints"));
     else Serial.println(F("-"));
   }
 
-  int no_arg_result_int() { strcpy(result_string, "100"); return 200; }
-  int no_arg_result_bool() { strcpy(result_string, "True"); return 200; }
-  int no_arg_result_str() { strcpy(result_string, "hello"); return 200; }
-  int no_arg_result_float() { strcpy(result_string, "1.1"); return 200; }
-  int no_arg_result_T() { strcpy(result_string, "4j2"); return 200; }
-
   bool exists(char * attribute) {
-    if (strcmp(attribute,"drive_forward_time_ms")==0) return true;
-    if (strcmp(attribute,"turn_time_ms")==0) return true;
-    if (strcmp(attribute,"ratio")==0) return true;
-    if (strcmp(attribute,"some_flag")==0) return true;
-    if (strcmp(attribute,"str_id")==0) return true;
     return false;
   }
 
   const char *get(char * attribute) {
-    if (strcmp(attribute,"drive_forward_time_ms")==0) { 
-      itoa (drive_forward_time_ms, temp_str, 10); 
-      return temp_str; 
-    }
-    if (strcmp(attribute,"turn_time_ms")==0) { 
-      itoa (turn_time_ms, temp_str, 10); 
-      return temp_str; 
-    }
-    if (strcmp(attribute,"str_id")==0) {
-      return str_id;
-    }
-    if (strcmp(attribute,"ratio")==0) { 
-      snprintf(temp_str,128,"%f",ratio);
-      return temp_str; 
-    }
-    if (strcmp(attribute,"some_flag")==0) { 
-      if (some_flag) return "True";
-      return "False";
-    }
     return "-";
   }
 
   int set(char* attribute, char* raw_value) {
-    if (strcmp(attribute,"drive_forward_time_ms")==0) {
-      int value = atoi(raw_value);
-      drive_forward_time_ms = value;
-      return 200;
-    }
-    if (strcmp(attribute,"turn_time_ms")==0) {
-      int value = atoi(raw_value);
-      turn_time_ms = value;
-      return 200;
-    }
-    if (strcmp(attribute,"ratio")==0) { 
-      double value = atof(raw_value);
-      ratio = value;
-      return 200;
-    }
-    if (strcmp(attribute,"some_flag")==0) {
-      if (strcmp(raw_value,"True")==0) {
-        some_flag=true;
-        return 200;
-      }
-      if (strcmp(raw_value,"False")==0) {
-        some_flag=false;
-        return 200;
-      }
-      return 500;
-    }
-    if (strcmp(attribute,"str_id")==0) { 
-      strncpy ( str_id, raw_value, 31 );
-      str_id[31] = '\0';   // null character manually added
-      return 200;
-    }
     return 404;
   }
 
@@ -185,15 +57,21 @@ public:
       itoa (value, result_string, 10);
       return 200;
   }
+  void eyeon(char *raw_value) {
+    eye_on(*raw_value);
+  }
+  void eyeoff(char *raw_value) {
+    eye_off(*raw_value);
+  }
+  void scrollstring(char *raw_value){
+    scroll_string(raw_value,100);
+  }
 
   int callfunc(char* funcname, char* raw_args) { 
     // Since this is a test host, it doesn't actually do anything
-    if (strcmp(funcname,"no_arg_result_int")==0) { no_arg_result_int(); return 200; }
-    if (strcmp(funcname,"no_arg_result_bool")==0) { no_arg_result_bool(); return 200; }
-    if (strcmp(funcname,"no_arg_result_str")==0) { no_arg_result_str(); return 200; }
-    if (strcmp(funcname,"no_arg_result_float")==0) { no_arg_result_float(); return 200; }
-    if (strcmp(funcname,"no_arg_result_T")==0) { no_arg_result_T(); return 200; }
-    if (strcmp(funcname,"one_arg_int_result_int")==0)  { one_arg_int_result_int(raw_args); return 200; }
+    if (strcmp(funcname,"eyeon")==0) { eyeon(raw_args); return 200; }
+    if (strcmp(funcname,"eyeoff")==0) { eyeoff(raw_args); return 200; }
+    if (strcmp(funcname,"scrollstring")==0)  { scrollstring(raw_args); return 200; }
 
     return 200;
   }
@@ -204,48 +82,14 @@ public:
   }
 };
 
-TestHost MyCommandHost;
+TetherHost MyCommandHost;
 
-// //----------------------------------------------------------
-// void setup()
-//{
-//  MyCommandHost.setup();
-//}
 
-//----------------------------------------------------------
-// void loop()
-// {
-//  MyCommandHost.run_host();
-// }
 
 
 unsigned long time;
 unsigned long lasttime;
 int buttondown;
-
-void ScrollSpriteStringSpriteExample_Test18() {
-    scroll_sprite(StringSprite("HELLO WORLD"),50);
-    delay(70);
-    scroll_sprite(StringSprite(":-) (:)"),50);
-    print_message("* * ", 100);
-    while(1) {
-        if (getButton('A') == PRESSED) break;
-        if (getButton('B') == PRESSED) {
-            buttondown = 1;
-        } else {
-            if (buttondown == 1) { // Button released -- may be nice to wrap this up
-                time = millis();
-                if (time-lasttime > 10) { // Debounce
-                    toggle_eye('A');
-                    toggle_eye('B');
-                    lasttime = time;
-                    buttondown = 0;
-                }
-            }
-        }
-        delay(50);
-    }
-}
 
 void setup()
 {    //.These next three should be merged into one
@@ -262,7 +106,6 @@ set_eye('R', HIGH);
 
 void loop()
 {
-    ScrollSpriteStringSpriteExample_Test18();
     MyCommandHost.run_host();
 }
 
