6398802be08b8eb4e34e4b04891b59e12155eede
Michael Sparks <sparks.m@gmail.com>
2014-11-24 00:30:11 +0000

    Change microbug/settings based on deployed host
    In particular, currently differentiate between michael's machine and pauls
    
    This is done by looking for a module called 'michaels_machine' and if that succeeds
    we use settings suitable for michael's dev machine, otherwise pauls.
    
    This will switch to match the server too shortly

--- website/app/microbug/settings.py
+++ website/app/microbug/settings.py
@@ -3,14 +3,21 @@
 # How long to wait for a file lock before giving up
 FILELOCK_TIMEOUT = 30
 
+try:
+    # Succeeds only on michael's machine :-)
+    import michaels_machine
+    BASE = "/home/michael/Work/CodeBug/MiniMicro/website/website/microbug_store"
+except ImportError:
+    BASE = '/Users/molt/Documents/microbug_store/primary'
+
 # Where we're keeping the immutable store of xml/py data in JSON
-PRIMARY_STORE_DIRECTORY = '/Users/molt/Documents/microbug_store/primary'
+PRIMARY_STORE_DIRECTORY = BASE + '/primary'
 
 # The pending queue for the compiler
-PENDING_PYTHON_QUEUE_DIRECTORY = '/Users/molt/Documents/microbug_store/pending'
+PENDING_PYTHON_QUEUE_DIRECTORY = BASE + '/pending'
 
 # The directory where compiled programs will be stored by the compiler process
-COMPILED_PYTHON_PROGRAMS_DIRECTORY = '/Users/molt/Documents/microbug_store/compiled'
+COMPILED_PYTHON_PROGRAMS_DIRECTORY = BASE + '/compiled'
 
 # The time, in seconds, to process each Python item
 PYTHON_ITEM_COMPILATION_TIME = 12
