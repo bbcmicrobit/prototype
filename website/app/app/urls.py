24f799ab080e50ed56fb1ceef4e319e29fb8a90d
Michael Sparks <sparks.m@gmail.com>
2014-11-24 00:26:17 +0000

    urls.py - autodetect that we're running on michael's dev machine
    This autodetects if the django instance is running on michael's development
    machine, and if it is it sets the URL routes appropriately. This is the first
    step towards deployment on the server.

--- website/app/app/urls.py
+++ website/app/app/urls.py
@@ -1,10 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Route to the Django admin interface.
-    url(r'^admin/', include(admin.site.urls)),
+try:
+    import michaels_machine
 
-    # Route to the Microbug application.
-    url(r'^microbug/', include("microbug.urls", namespace='microbug')),
-)
+    urlpatterns = patterns('',
+        # Route to the Django admin interface.
+        url(r'^admin/', include(admin.site.urls)),
+
+        # Route to the Microbug application.
+        url(r'^', include("microbug.urls", namespace='microbug')),
+    )
+
+except ImportError:
+
+    urlpatterns = patterns('',
+        # Route to the Django admin interface.
+        url(r'^admin/', include(admin.site.urls)),
+
+        # Route to the Microbug application.
+        url(r'^microbug/', include("microbug.urls", namespace='microbug')),
+    )
