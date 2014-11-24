312f07746d435b31af161af41d6fe06882118c81
Michael Sparks <sparks.m@gmail.com>
2014-11-24 00:24:41 +0000

    Django 1.6 does not have django.contrib.auth.middleware.SessionAuthenticationMiddleware
    The reason Django 1.6 matters is because Ubuntu 14.04.1LTS - the version of Ubuntu used on the server - only has Django 1.6 packaged

--- website/app/app/settings.py
+++ website/app/app/settings.py
@@ -45,10 +45,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 
 ROOT_URLCONF = 'app.urls'
 
