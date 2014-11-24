fd7fb3a87462178395181d041e1c2f7a9c41b37b
Michael Sparks <sparks.m@gmail.com>
2014-11-24 03:23:20 +0100

    Detect running on the shared dev server

--- website/app/app/urls.py
+++ website/app/app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 try:
+    # Are we running on michael's dev machine?
     import michaels_machine
 
     urlpatterns = patterns('',
@@ -13,11 +14,27 @@ try:
     )
 
 except ImportError:
+    try:
+        # Are we running on the dev server?
+        import sparkslabs
 
-    urlpatterns = patterns('',
-        # Route to the Django admin interface.
-        url(r'^admin/', include(admin.site.urls)),
+        urlpatterns = patterns('',
+            # Route to the Django admin interface.
+            url(r'^admin/', include(admin.site.urls)),
 
-        # Route to the Microbug application.
-        url(r'^microbug/', include("microbug.urls", namespace='microbug')),
-    )
+            # Route to the Microbug application.
+            url(r'^', include("microbug.urls", namespace='microbug')),
+        )
+
+    except ImportError:
+        # No. Assume we're running on Paul's dev machine in standalone mode.
+
+
+
+        urlpatterns = patterns('',
+            # Route to the Django admin interface.
+            url(r'^admin/', include(admin.site.urls)),
+
+            # Route to the Microbug application.
+            url(r'^microbug/', include("microbug.urls", namespace='microbug')),
+        )
