dbc9997b54058872d5bd12fd690a1fb463829fe6
Michael Sparks <sparks.m@gmail.com>
2015-01-04 18:26:57 +0000

    Changes to development version of main_test

--- compiler/py2cc/main.py
+++ compiler/py2cc/main.py
@@ -34,36 +34,39 @@ def main_test(files):
         print
         print "PARSING", filename
         source = open("tests/progs/"+filename).read()
+        print source
 
         x = parse(source, lexer)
 
         pprint.pprint(x,width=120)
         print "-"*120
-        y  = gen_code(x)
-        basedir = "tests/genprogs/"+filename
-        os.mkdir(basedir)
-        f = open(basedir+"/user_code.ino", "w")
-        f.write(y)
-        f.close()
+        if 1:
+            y  = gen_code(x)
+            basedir = "tests/genprogs/"+filename
+            os.mkdir(basedir)
+            f = open(basedir+"/user_code.ino", "w")
+            f.write(y)
+            f.close()
 
-        shutil.copyfile("../dal/dal.h", basedir+"/dal.h")
-        shutil.copyfile("../dal/Makefile", basedir+"/Makefile")
-        shutil.copyfile("../dal/Makefile_arduino", basedir+"/Makefile_arduino")
-        shutil.copyfile("../dal/spark_font.h", basedir+"/spark_font.h")
-        shutil.copyfile("../dal/atmel_bootloader.h", basedir+"/atmel_bootloader.h")
-        here = os.getcwd()
-        os.chdir(basedir)
-        os.system("make")
-        print "READY TO UPLOAD"
-        print "PRESS RETURN TO ERASE"
-        raw_input(">")
-        os.system("make erase")
-        print "PRESS RETURN TO FLASH DEVICE"
-        raw_input(">")
-        os.system("make flash_device")
-        os.chdir(here)
+        if 1:
+            shutil.copyfile("../dal/dal.h", basedir+"/dal.h")
+            shutil.copyfile("../dal/Makefile", basedir+"/Makefile")
+            shutil.copyfile("../dal/Makefile_arduino", basedir+"/Makefile_arduino")
+            shutil.copyfile("../dal/spark_font.h", basedir+"/spark_font.h")
+            shutil.copyfile("../dal/atmel_bootloader.h", basedir+"/atmel_bootloader.h")
+            here = os.getcwd()
+            os.chdir(basedir)
+            os.system("make")
+            print "READY TO UPLOAD"
+            print "PRESS RETURN TO ERASE"
+            raw_input(">")
+            os.system("make erase")
+            print "PRESS RETURN TO FLASH DEVICE"
+            raw_input(">")
+            os.system("make flash_device")
+            os.chdir(here)
 
-        print "#"*120
+            print "#"*120
 
 def main_single(source_file, dest_file, tmp_directory, cleanup=False):
     """
