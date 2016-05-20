388413011816e31aeb2f00901e168db6a8ea1098
Michael Sparks <sparks.m@gmail.com>
2016-05-20 14:46:22 +0100

    Added license info to installer subsystem

--- installer/mac_os_x/makedmg.sh
+++ installer/mac_os_x/makedmg.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+#
+# Copyright 2016 British Broadcasting Corporation and Contributors(1)
+#
+# (1) Contributors are listed in the AUTHORS file (please extend AUTHORS,
+#     not this header)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance this license (or the alternative
+# license below).
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is also alternatively licensed under the terms of the GPL
+# version 2. You may obtain a copy of the license at:
+#
+#     http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 echo "Cleaning targets..."
 rm -rf build dist libs BBCBug.dmg
--- installer/win/PY2EXE/make.bat
+++ installer/win/PY2EXE/make.bat
@@ -1,3 +1,28 @@
+REM
+REM Copyright 2016 British Broadcasting Corporation and Contributors(1)
+REM
+REM (1) Contributors are listed in the AUTHORS file (please extend AUTHORS,
+REM     not this header)
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License"); you may
+REM not use this file except in compliance this license (or the alternative
+REM license below).
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM This file is also alternatively licensed under the terms of the GPL
+REM version 2. You may obtain a copy of the license at:
+REM
+REM     http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM
+
 rd /s /q dist
 rd /s /q build
 C:\Python27\python setup.py py2exe
